1fa1a50b101ca7becd90d8583c7ebe682442b17f
qitas <qitas@qitas.cn>
2019-09-09 22:01:15 +0800

    fix define

--- README.md
+++ README.md
@@ -4,7 +4,7 @@
 [EDGE-Q](https://github.com/OS-Q/EDGE-Q) -> Q[4] -> M[12] -> W[52] -> D[365] -> H[24] -> N[60] -> S[60]
 ### [Platform描述](https://github.com/OS-Q/H1/wiki) 
 
-[Platform H1](https://github.com/OS-Q/H1)基于PlatformIO开发平台STM32应用
+[Platform H1](https://github.com/OS-Q/H1)基于PlatformIO开发平台
 
 ### [关联资源](https://github.com/OS-Q/)
 
--- platform.py
+++ platform.py
@@ -12,9 +12,9 @@ class Stm32Platform(PlatformBase):
                 "build.core", "arduino"))
 
         if "arduino" in variables.get("pioframework", []) and build_core == "maple":
-            self.frameworks['arduino']['package'] = "framework-arduinoststm32-maple"
-            self.packages["framework-arduinoststm32-maple"]["optional"] = False
-            self.packages["framework-arduinoststm32"]["optional"] = True
+            self.frameworks['arduino']['package'] = "framework-stm32maple"
+            self.packages["framework-stm32maple"]["optional"] = False
+            self.packages["framework-stm32arduino"]["optional"] = True
 
         if variables.get("upload_protocol", "") == "dfu":
             self.packages["tool-dfuutil"]["optional"] = False
