e8e164ff1b21104d7dfc841de76ab10cabd7d980
qitas <qitas@qitas.cn>
2019-10-31 10:56:52 +0800

    fix f4 to f1

--- examples/stm32cube-ll-blink/src/main.c
+++ examples/stm32cube-ll-blink/src/main.c
@@ -1,12 +1,12 @@
-#include "stm32f4xx_ll_bus.h"
-#include "stm32f4xx_ll_rcc.h"
-#include "stm32f4xx_ll_system.h"
-#include "stm32f4xx_ll_utils.h"
-#include "stm32f4xx_ll_gpio.h"
+#include "stm32f1xx_ll_bus.h"
+#include "stm32f1xx_ll_rcc.h"
+#include "stm32f1xx_ll_system.h"
+#include "stm32f1xx_ll_utils.h"
+#include "stm32f1xx_ll_gpio.h"
 
-#define LED_PIN                 LL_GPIO_PIN_5
-#define LED_GPIO_PORT           GPIOA
-#define LED_GPIO_CLK_ENABLE()   LL_AHB1_GRP1_EnableClock(LL_AHB1_GRP1_PERIPH_GPIOA)
+#define LED_PIN                 LL_GPIO_PIN_13
+#define LED_GPIO_PORT           GPIOC
+#define LED_GPIO_CLK_ENABLE()   LL_APB2_GRP1_EnableClock(LL_APB2_GRP1_PERIPH_GPIOC)
 
 void Configure_GPIO(void)
 {
@@ -24,30 +24,46 @@ void SystemClock_Config(void)
   };
 
   /* Set FLASH latency */
-  LL_FLASH_SetLatency(LL_FLASH_LATENCY_3);
+  LL_FLASH_SetLatency(LL_FLASH_LATENCY_2);
+  //LL_FLASH_SetLatency(LL_FLASH_LATENCY_3);
 
   /* Main PLL configuration and activation */
-  LL_RCC_PLL_ConfigDomain_SYS(LL_RCC_PLLSOURCE_HSE, LL_RCC_PLLM_DIV_8, 400, LL_RCC_PLLP_DIV_4);
+  LL_RCC_PLL_ConfigDomain_SYS(LL_RCC_PLLSOURCE_HSE_DIV_1, LL_RCC_PLL_MUL_9);
   LL_RCC_PLL_Enable();
+  // LL_RCC_PLL_ConfigDomain_SYS(LL_RCC_PLLSOURCE_HSE, LL_RCC_PLLM_DIV_8, 400, LL_RCC_PLLP_DIV_4);
+  // LL_RCC_PLL_Enable();
   while(LL_RCC_PLL_IsReady() != 1)
   {
   };
-
-  /* Sysclk activation on the main PLL */
   LL_RCC_SetAHBPrescaler(LL_RCC_SYSCLK_DIV_1);
-  LL_RCC_SetSysClkSource(LL_RCC_SYS_CLKSOURCE_PLL);
-  while(LL_RCC_GetSysClkSource() != LL_RCC_SYS_CLKSOURCE_STATUS_PLL)
-  {
-  };
-
-  /* Set APB1 & APB2 prescaler */
   LL_RCC_SetAPB1Prescaler(LL_RCC_APB1_DIV_2);
   LL_RCC_SetAPB2Prescaler(LL_RCC_APB2_DIV_1);
+  LL_RCC_SetSysClkSource(LL_RCC_SYS_CLKSOURCE_PLL);
+
+   /* Wait till System clock is ready */
+  while(LL_RCC_GetSysClkSource() != LL_RCC_SYS_CLKSOURCE_STATUS_PLL)
+  {
+  
+  }
+
+  // LL_SYSTICK_SetClkSource(LL_SYSTICK_CLKSOURCE_HCLK);
+  LL_SetSystemCoreClock(72000000);
+  LL_Init1msTick(72000000);
+  /* Sysclk activation on the main PLL */
+  // LL_RCC_SetAHBPrescaler(LL_RCC_SYSCLK_DIV_1);
+  // LL_RCC_SetSysClkSource(LL_RCC_SYS_CLKSOURCE_PLL);
+  // while(LL_RCC_GetSysClkSource() != LL_RCC_SYS_CLKSOURCE_STATUS_PLL)
+  // {
+  // };
+
+  /* Set APB1 & APB2 prescaler */
+  // LL_RCC_SetAPB1Prescaler(LL_RCC_APB1_DIV_2);
+  // LL_RCC_SetAPB2Prescaler(LL_RCC_APB2_DIV_1);
 
   /* Update CMSIS variable (which can be updated also through SystemCoreClockUpdate function) */
-  SystemCoreClock = SYS_CLOCK;
+// SystemCoreClock = SYS_CLOCK;
   
-  LL_Init1msTick(SystemCoreClock);
+  // LL_Init1msTick(SystemCoreClock);
 }
 
 
