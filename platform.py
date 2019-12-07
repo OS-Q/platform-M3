87d1c3995b832db2f26e9ac83a91c1bed6b2e5e1
qitas <qitas@qitas.cn>
2019-12-07 15:53:23 +0800

    fix py

--- platform.py
+++ platform.py
@@ -4,7 +4,7 @@ from platform import system
 from platformio.managers.platform import PlatformBase
 from platformio.util import get_systype
 
-class Ststm32Platform(PlatformBase):
+class H1Platform(PlatformBase):
 
     def configure_default_packages(self, variables, targets):
         board = variables.get("board")
