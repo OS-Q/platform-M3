da0aa6cfaf34c34763970c62fc912c1da11686b8
qitas <qitas@qitas.cn>
2021-03-29 08:46:55 +0800

    fix

--- builder/frameworks/stm32cube.py
+++ builder/frameworks/stm32cube.py
@@ -43,7 +43,7 @@ PRODUCT_LINE = board.get("build.product_line", "")
 assert PRODUCT_LINE, "Missing MCU or Product Line field"
 
 FRAMEWORK_DIR = platform.get_package_dir("framework-stm32cube%s" % MCU[5:7])
-LDSCRIPTS_DIR = platform.get_package_dir("E21L")
+LDSCRIPTS_DIR = platform.get_package_dir("tool-ldscripts-ststm32")
 assert all(os.path.isdir(d) for d in (FRAMEWORK_DIR, LDSCRIPTS_DIR))
 
 
