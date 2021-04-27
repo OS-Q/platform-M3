f99cd66df59899042b6ececee2c2270bc9f7e515
qitas <qitas@qitas.cn>
2021-04-27 11:16:28 +0800

    init

---
+++ extend/frameworks/mbed.py
@@ -0,0 +1,10 @@
+from os.path import join
+
+from SCons.Script import Import, SConscript
+
+Import("env")
+
+# https://github.com/platformio/builder-framework-mbed.git
+SConscript(
+    join(env.PioPlatform().get_package_dir("framework-mbed"), "platformio",
+         "platformio-build.py"))
